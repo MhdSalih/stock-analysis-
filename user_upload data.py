362215032286a396ae1b442d8ac5b842d97cc3fc
MhdSalih <92842416+MhdSalih@users.noreply.github.com>
2021-12-22 13:46:16 +0530

    Update user_upload data.py

--- user_upload data.py
+++ user_upload data.py
@@ -17,36 +17,44 @@ upload_file=st.sidebar.file_uploader(label="upload your csv or excel file",type=
 
 global data
 if upload_file is not None:
-  print(upload_file)
-  print('hello')
-  try:
-    data = pd.read_csv(upload_file)
-  except Exception as e:
-    print(e)
-    data = pd.read_excel(upload_file)
+    print(upload_file)
+    print('hello')
+    try:
+        data = pd.read_csv(upload_file)
+    except Exception as e:
+        print(e)
+        data = pd.read_excel(upload_file)
     
-   data["VOCDATE"] = pd.to_datetime(data.VOCDATE)
+        data["VOCDATE"] = pd.to_datetime(data.VOCDATE)
 
-   df = data[data['VOCDATE'].dt.year == 2020]
-   df=df[df["salesanalysis1"]==1]
-   df.Design= df.Design.str.lower()
-   df["Design"]=df["Design"].astype('category')
-   df["QUANTITY"]=1
-   df2=df[["VOCNO","Design","VOCDATE",'QUANTITY']]
+        df = data[data['VOCDATE'].dt.year == 2020]
+        df=df[df["salesanalysis1"]==1]
+        df.Design= df.Design.str.lower()
+        df["Design"]=df["Design"].astype('category')
+        df["QUANTITY"]=1
+        df2=df[["VOCNO","Design","VOCDATE",'QUANTITY']]
 
 
-   basket=df2.groupby(["VOCNO","Design"])["QUANTITY"].sum().unstack().reset_index().fillna(0).set_index("VOCNO")
-   basket=pd.DataFrame(basket)
+        basket=df2.groupby(["VOCNO","Design"])["QUANTITY"].sum().unstack().reset_index().fillna(0).set_index("VOCNO")
+        basket=pd.DataFrame(basket)
 
-   def encode_unit(x):
-      if x <= 0:
-         return 0
-      if x >= 1:
-         return 1
-   
-   basket_set = basket.applymap(encode_unit)
-   frequent_itemsets = apriori(basket_set, min_support=0.08, use_colnames=True)
-   rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
-   rules["antecedents"] = rules["antecedents"].apply(lambda x: ', '.join(list(x))).astype("unicode")
-   rules["consequents"] = rules["consequents"].apply(lambda x: ', '.join(list(x))).astype("unicode")
-   st.write(rules.head(40))
+        def encode_unit(x):
+            if x <= 0:
+                return 0
+            if x >= 1:
+                return 1
+    
+        basket_set = basket.applymap(encode_unit)
+
+        frequent_itemsets = apriori(basket_set, min_support=0.08, use_colnames=True)
+#print (frequent_itemsets)
+        rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
+        rules["antecedents"] = rules["antecedents"].apply(lambda x: ', '.join(list(x))).astype("unicode")
+        rules["consequents"] = rules["consequents"].apply(lambda x: ', '.join(list(x))).astype("unicode")
+#frequent_itemsets
+
+# In[16]:
+
+
+        st.write(rules.head(40))
+
