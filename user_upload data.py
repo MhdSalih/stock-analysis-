ff43fd96b1d2ddc5b8e06dd7b1a344cf496391b7
MhdSalih <92842416+MhdSalih@users.noreply.github.com>
2021-12-23 18:41:59 +0530

    Update user_upload data.py

--- user_upload data.py
+++ user_upload data.py
@@ -26,8 +26,7 @@ if upload_file is not None:
 
     df["Design"]= df.Design.str.lower()
     df["Design"]=df["Design"].astype('category')
-    df=df.replace({'Category_Code':new})
-    df['Design'] = np.where(df['Design']== "diamond",df["Category_Code"] , df['Design'])
+    #df['Design'] = np.where(df['Design']== "diamond",df["Category_Code"] , df['Design'])
     df["QUANTITY"]=1
     df2=df[["VOCNO","Design",'QUANTITY']]
 
