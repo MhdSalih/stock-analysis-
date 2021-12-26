7508c7b7e463b092c66d46f4860cf95ea143ef83
MhdSalih <92842416+MhdSalih@users.noreply.github.com>
2021-12-26 11:53:03 +0530

    Update user_upload data.py

--- user_upload data.py
+++ user_upload data.py
@@ -9,7 +9,8 @@ from PIL import Image
 # In[15]:
 image = Image.open('WhatsApp-Image-2021-12-23-at-3.05.01-PM-_1_.jpg')
 image1=Image.open("WhatsApp-Image-2021-12-23-at-3.05.01-PM-_2_.jpg")
-st.image(image,image1)
+st.image(image)
+st.image(image1)
 st.title('Market Basket Analysis')
 st.write("Market basket analysis is a data mining technique used by retailers to increase sales by better understanding customer purchasing patterns. It involves analyzing large data sets, such as purchase history, to reveal product groupings, as well as products that are likely to be purchased together.")
 
