e78cffad4bde93d24273e6f93198c84d3a71967c
MhdSalih <92842416+MhdSalih@users.noreply.github.com>
2021-12-31 15:05:13 +0530

    Update user_upload data.py

--- user_upload data.py
+++ user_upload data.py
@@ -88,7 +88,7 @@ if upload_file is not None:
     rules.columns = map(str.upper, rules.columns)
     st.markdown(
     """<style>
-        .rules {text-align: center !important}
+        .dataframe {text-align: center !important}
     </style>
     """, unsafe_allow_html=True) 
     
