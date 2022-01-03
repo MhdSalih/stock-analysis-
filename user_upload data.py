8ef8bfc9de56b23aaa9ef48b954f5cae017c5997
MhdSalih <92842416+MhdSalih@users.noreply.github.com>
2022-01-03 15:12:22 +0530

    Update user_upload data.py

--- user_upload data.py
+++ user_upload data.py
@@ -16,7 +16,7 @@ center.write("")
 last.image(image1)
 
 if st.button('download'):
-    data.head(10)
+    st.write(data.head(10))
 
 st.title('Market Basket Analysis')
 st.write("Market basket analysis is a data mining technique used by retailers to increase sales by better understanding customer purchasing patterns. It involves analyzing large data sets, such as purchase history, to reveal product groupings, as well as products that are likely to be purchased together.")
