ccb4df975f3b6a0811df90e40e9454b40614f00d
MhdSalih <92842416+MhdSalih@users.noreply.github.com>
2022-01-04 13:59:01 +0530

    Update user_upload data.py

--- user_upload data.py
+++ user_upload data.py
@@ -26,9 +26,10 @@ last.image(image1)
 st.title('Market Basket Analysis')
 st.write("Market basket analysis is a data mining technique used by retailers to increase sales by better understanding customer purchasing patterns. It involves analyzing large data sets, such as purchase history, to reveal product groupings, as well as products that are likely to be purchased together.")
 st.write("----------------------------------------------------------------------------------------")
-st.write("antecedent  : item found within the data")
-st.write("consequent  : item found in combination with the antecedent") 
-st.write("support     : the frequencey of occurane of the item")
+st.write({"antecedent  : item found within the data",
+          "consequent  : item found in combination with the antecedent",
+          "support     : the frequencey of occurane of the item"
+         })
 new={'PN':"diamond pendant",
 'RN':"diamond ring",
 'BT':"diamond bracelet",
