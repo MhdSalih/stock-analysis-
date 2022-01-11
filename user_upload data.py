65d7e39b1bcbf4ea03daef315c7391b477e77cb2
MhdSalih <92842416+MhdSalih@users.noreply.github.com>
2022-01-11 10:53:45 +0530

    Update user_upload data.py

--- user_upload data.py
+++ user_upload data.py
@@ -145,7 +145,7 @@ if upload_file is not None:
 
     # Generate parallel coordinates plot
     
-    fig=plt.figure(figsize=(4,8))
+    fig=plt.figure(figsize=(2,4))
     parallel_coordinates(coords, 'rule')
     plt.legend([])
     plt.grid(True)
